0eb164d72019d31e3cf9daf7c8b783e54d5e9989
Padoari <padoari85@gmail.com>
2023-11-11 13:09:53 +0900

    Initial Commit

---
+++ Beautiful Universe/Beautiful_UniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beautiful_UniverseApp.swift
+//  Beautiful Universe
+//
+//  Created by jeongyun on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Beautiful_UniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
